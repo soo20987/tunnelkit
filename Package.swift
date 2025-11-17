bdf4ab5d17b47a6ae0ba3277063883b823639a5e
soo20987 <76559934+soo20987@users.noreply.github.com>
2025-11-17 10:11:39 +0900

    Add wg-go-apple dependency from master branch

--- Package.swift
+++ Package.swift
@@ -40,6 +40,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver", from: "1.9.0"),
         .package(url: "https://github.com/passepartoutvpn/openssl-apple", from: "1.1.11500"),
+        .package(url: "https://github.com/soo20987/wg-go-apple.git", .branch("master"))
         //.package(name: "WireGuardKit", url: "https://git.zx2c4.com/wireguard-apple", .exact("1.0.15-26"))
         //.package(name: "WireGuardKit", url: "https://github.com/soo20987/wireguard-apple", .branch("master"))
         //.package(name: "WireGuardKit", url: "https://github.com/passepartoutvpn/wireguard-apple", from: "1.0.16")

d02db9e5fa6e02e2978d33fa4b4dfd45e3c91e79
soo20987 <76559934+soo20987@users.noreply.github.com>
2025-11-17 14:14:12 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -40,8 +40,8 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver", from: "1.9.0"),
         //.package(url: "https://github.com/passepartoutvpn/openssl-apple", from: "1.1.11500")
-        .package(url: "https://github.com/passepartoutvpn/openssl-apple", from: "1.1.11700")
-        //.package(url: "https://github.com/soo20987/wg-go-apple", .branch("master"))
+        .package(url: "https://github.com/passepartoutvpn/openssl-apple", from: "1.1.11700"),
+        .package(url: "https://github.com/soo20987/wg-go-apple", .branch("master"))
         //.package(name: "WireGuardKit", url: "https://git.zx2c4.com/wireguard-apple", .exact("1.0.15-26"))
         //.package(name: "WireGuardKit", url: "https://github.com/soo20987/wireguard-apple", .branch("master"))
         //.package(name: "WireGuardKit", url: "https://github.com/passepartoutvpn/wireguard-apple", from: "1.0.16")
